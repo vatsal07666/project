bec84ca09d9cc63a7ae2adafa914845137e64e95
V S <vsdeveloper@gmail.com>
2026-02-10 10:45:17 +0530

    razorpay script

---
+++ src/Payment/LoadRazorpay.jsx
@@ -0,0 +1,16 @@
+const LoadRazorpay = () => {
+    return new Promise((resolve) => {
+        if (window.Razorpay) {
+            resolve(true);
+            return;
+        }
+
+        const script = document.createElement("script");
+        script.src = "https://checkout.razorpay.com/v1/checkout.js";
+        script.onload = () => resolve(true);
+        script.onerror = () => resolve(false);
+        document.body.appendChild(script);
+    });
+};
+
+export default LoadRazorpay;
